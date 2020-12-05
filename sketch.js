335c209b0f1de5cc7c2829e3eb69ce869343d0fd
Narendra <parandku@gmail.com>
2020-12-05 11:54:07 +0530

    constraint added

--- sketch.js
+++ sketch.js
@@ -1,21 +1,26 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
+  
     engine = Engine.create();
     world = engine.world;
 
-
+  //  constrainedLog = new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -27,7 +32,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+ 
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -35,6 +40,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constrainedLog = new Log(230,180,80,PI/2);
+   
+    chain = new Chain(bird.body, constrainedLog.body);
 
 }
 
@@ -44,6 +52,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
     box1.display();
     box2.display();
     ground.display();
@@ -61,4 +70,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    chain.display();
+    
 }
